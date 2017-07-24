bed76f68f3d18b815b2257ce2f74c8c5c31b0f89
Logan Freesh <Logancf@gmail.com>
2017-07-24 15:32:28 -0700

    AI overhaul, different kinds of nets can now learn from each other.

--- build.bat
+++ build.bat
@@ -10,8 +10,12 @@ cd..
 
 
 
-%dmd% src\spaceships.d src\steering.d src\collision.d src\unit.d src\factory_unit.d src\capture_point.d src\team.d src\team_manual.d src\team_random.d src/team_scripted_capper.d src/team_scripted_defender.d src/team_mod_reinforcement.d src\ai_base.d src\ai_command.d src\ai_build.d src\ai_base_mod_reinforcement.d src\ai_command_mod_reinforcement.d src\ai_build_mod_reinforcement.d src\network_input_display.d src\matchinfo.d -profile=gc -debug -gc -unittest -IDSFML_2.1_DMD_2.086.2_32bits\import\ -Iand\ -L+DSFML_2.1_DMD_2.086.2_32bits\lib\ -L+DSFML_2.1_DMD_2.086.2_32bits\lib\ dsfml-graphics.lib dsfml-window.lib dsfml-system.lib dsfmlc-graphics.lib dsfmlc-window.lib dsfmlc-system.lib and\api.lib and\neuralnetwork.lib
+%dmd% src\spaceships.d src\steering.d src\collision.d src\unit.d src\factory_unit.d src\capture_point.d ^
+src\team.d src\team_manual.d src\team_random.d src\team_scripted_capper.d src\team_scripted_defender.d src\team_mod_reinforcement.d ^
+src\ai_base.d src\ai_command.d src\ai_build.d  ^
+src\record_keeper.d src\nn_manager.d src\nn_manager_classifier.d src\nn_manager_mod_reinforcement.d src\nn_manager_copycat.d src\network_input_display.d src\matchinfo.d ^
+-profile=gc -debug -gc -unittest -IDSFML_2.1_DMD_2.086.2_32bits\import\ -Iand\ -L+DSFML_2.1_DMD_2.086.2_32bits\lib\ -L+DSFML_2.1_DMD_2.086.2_32bits\lib\ dsfml-graphics.lib dsfml-window.lib dsfml-system.lib dsfmlc-graphics.lib dsfmlc-window.lib dsfmlc-system.lib and\api.lib and\neuralnetwork.lib
 
 
 endlocal
-
+echo "Build Complete!"
